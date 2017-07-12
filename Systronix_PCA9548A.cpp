b237991d0787395134506354884be473ebad66e0
wskendall <skendall@systronix.com>
2017-07-12 17:27:30 -0600

    rewrite to make Wire1 aware; to conform to the 'standard' established in PCA9557;

--- Systronix_PCA9548A.cpp
+++ Systronix_PCA9548A.cpp
@@ -7,6 +7,8 @@
 
 /**---------------------------< REVISIONS >----------------------------------
 
+2017 Jul 12			rewrite to make Wire1 aware; to conform to the 'standard' established in PCA9557
+
 2017 May 22 bboyes	change to UINT64_MAX
 
 2017 May 21 bboyes	Changed total_error_count and successful_count to 64 bit 
@@ -15,7 +17,7 @@ long long and check against ULLONG_MAX before incrementing
 2017 May 05 bboyes	Changed default i2c_t3 timeout to 5000 usec, which is still
 likely way too much. There's nothing that slow in the 9557 access.
 
-2017 Mar 15	bboyes	Changed tally_errors to properly record current Wire.status()
+2017 Mar 15	bboyes	Changed tally_transaction to properly record current Wire.status()
 return values. Disabled simpleTest because it is not so simple and could return
 with the config register changed in the middle of another loop which requires it
 to be not changed.
@@ -32,7 +34,7 @@ to be not changed.
 	base is clipped to min or max if necessary
 
 -----------------------------------------------------------------------------*/
-
+/*
 Systronix_PCA9548A::Systronix_PCA9548A(uint8_t base)
 	{
 	if (base < PCA9548A_SLAVE_ADDR_0) 
@@ -52,9 +54,13 @@ Systronix_PCA9548A::Systronix_PCA9548A(uint8_t base)
 		}
 	error.total_error_count = 0;				// clear the error counter
 	}
+*/
 
-
+//---------------------------< D E F A U L T   C O N S R U C T O R >------------------------------------------
+//
 // default constructor assumes lowest base address
+//
+
 Systronix_PCA9548A::Systronix_PCA9548A()
 	{
 	_base = PCA9548A_SLAVE_ADDR_0;
@@ -63,21 +69,33 @@ Systronix_PCA9548A::Systronix_PCA9548A()
 	}
 
 
+//---------------------------< D E S T R U C T O R >----------------------------------------------------------
+//
 // destructor
+//
+
 Systronix_PCA9548A::~Systronix_PCA9548A()
 {
 	// Anything to do here? Leave I2C as master? Set flag?
 }
 
-/**
-	Variables publicly read-only, privately read/write
-*/
 
-// base address passed in constructor was out of range so clipped to value min <= base <= max
+//---------------------------< B A S E _ C L I P P E D >------------------------------------------------------
+//
+// base address passed in constructor or setup() was out of range so clipped to value min <= base <= max
+//
+
 boolean Systronix_PCA9548A::base_clipped()
 {
 	return _base_clipped;
 }
+
+
+//---------------------------< B A S E _ G E T >--------------------------------------------------------------
+//
+// return the base address; will be the same as the address in constructor or setup() unless clipped in which
+// case base address shall be PCA9548A_SLAVE_ADDR_0 or PCA9548A_SLAVE_ADDR_7
+//
 
 uint8_t Systronix_PCA9548A::base_get()
 {
@@ -85,23 +103,54 @@ uint8_t Systronix_PCA9548A::base_get()
 }
 
 
-//---------------------------< B E G I N >--------------------------------------------------------------------
-// One-time startup things here. Call this only at program start.
-/*!
-	@brief  Join the I2C bus as a master
-*/
+//---------------------------< S E T U P >--------------------------------------------------------------------
+//
+// TODO: merge with begin()? This function doesn't actually do anything, it just sets some private values. It's
+// redundant and some params must be effectively specified again in begin (Wire net and pins are not independent).	what parameters are specified again? [wsk]
+//
 
-void Systronix_PCA9548A::begin(void)
+uint8_t Systronix_PCA9548A::setup (uint8_t base, i2c_t3 wire, char* name)
 	{
-	Wire.begin();			// join I2C as master
-#if defined I2C_T3_H	
-	Wire.setDefaultTimeout(5000);	// 1000 usec = 1 msec; 10,000 = 10 msec seems too long.
-#endif
+	if ((PCA9548A_BASE_MIN > base) || (PCA9548A_BASE_MAX < base))
+		{
+		tally_transaction (SILLY_PROGRAMMER);
+		return FAIL;
+		}
 
+	_base = base;
+	_wire = wire;
+	_wire_name = wire_name = name;		// protected and public
+	return SUCCESS;
 	}
 
 
-/**---------------------------< I N I T >----------------------------------------------------------------------
+//---------------------------< B E G I N >--------------------------------------------------------------------
+//
+//
+//
+
+void Systronix_PCA9548A::begin (i2c_pins pins, i2c_rate rate)
+	{
+	_wire.begin(I2C_MASTER, 0x00, pins, I2C_PULLUP_EXT, rate);	// join I2C as master
+	Serial.printf("95548A Lib begin %s\r\n", _wire_name);
+	_wire.setDefaultTimeout(200000); // 200ms
+	}
+
+
+//---------------------------< D E F A U L T   B E G I N >----------------------------------------------------
+/**
+	@ brief Void version of begin for backwards compatibility, assumes Wire net
+
+*/
+
+void Systronix_PCA9548A::begin (void)
+	{
+	_wire.begin();	// join I2C as master
+	_wire.setDefaultTimeout(200000); // 200ms
+	}
+
+
+/**---------------------------< I N I T >---------------------------------------------------------------------
 
 Address the MUX and write to the control register.  
 If successful, _exists = true;
@@ -115,52 +164,124 @@ TODO How is this different from control write ???
 
 uint8_t Systronix_PCA9548A::init (uint8_t control)
 	{
-	
-	Wire.beginTransmission (_base);			// base address
-	// no return to check
+	uint8_t ret_val;
 
-	error.ret_val = Wire.write (control);		// write to control reg
-	// return: #bytes written = success, 0=fail
-	if (1 != error.ret_val)
-		{
-		error.ret_val = 0;
-		tally_errors (error.ret_val);			// buffer write length error
-		// now if error, error.ret_val is 0
-		// and no point in continuing
-		}
-	else
-		{
-		// no error in buffer write so proceed
-		error.ret_val = Wire.endTransmission();
-		// return: 0=success, 1=data too long, 2=recv addr NACK, 3=recv data NACK, 4=other error	
-	  	if (error.ret_val)
-			{					
-			// unsuccessful i2c transaction, get more detail
-			// Serial.printf("init endTransmission failed with return of 0x%.2X\r\n", error.ret_val);
-#if defined I2C_T3_H				
-			error.ret_val = Wire.status();			// detailed status value enum 0..10
-			// return = I2C_WAITING, I2C_SENDING, I2C_SEND_ADDR, I2C_RECEIVING, I2C_TIMEOUT, I2C_ADDR_NAK, I2C_DATA_NAK, I2C_ARB_LOST, I2C_BUF_OVF, I2C_SLAVE_TX, I2C_SLAVE_RX
-			// Serial.printf("init endTransmission status value 0x%.2X\r\n", error.ret_val);
-#endif			
-			tally_errors (error.ret_val);							// increment the appropriate counter
-			}
-		else
-			{
-			// completely successful
-			if (error.successful_count < UINT64_MAX) error.successful_count++;
-			_control_reg = control;					// shadow copy to remember this setting
-			return SUCCESS;
-			}
-		}
-										
-	return !SUCCESS;
+	error.exists = true;					// so we can use control_write(); we'll find out later if device does not exist
 
-	// TODO this could return with error.ret_val set to zero
+//	Serial.printf("Lib init %s at base 0x%.2X\r\n", _wire_name, _base);
+	ret_val = control_write (control);		// if successful this means we got two ACKs from slave device
+	if (SUCCESS != ret_val)
+		{
+		Serial.printf("Lib init %s at base 0x%.2X failed with 0x%.2X\r\n", _wire_name, _base, error.error_val);
+		error.exists = false;				// only place error.exists is set false
+		return ABSENT;
+		}
+
+	tally_transaction (SUCCESS);
+	return SUCCESS;
 	}
 
 
-/**---------------------------< C O N T R O L W R I T E >----------------------------------------------------
+//---------------------------< R E S E T _ B U S >------------------------------------------------------------
+/**
+	Invoke resetBus of whichever Wire net this class instance is using
+	@return nothing
+*/
+void Systronix_PCA9548A::reset_bus (void)
+{
+	_wire.resetBus();
+}
 
+
+//---------------------------< R E S E T _ B U S _ C O U N T _ R E A D >--------------------------------------
+/**
+	Return the resetBusCount of whichever Wire net this class instance is using
+	@return number of Wire net resets, clips at UINT32_MAX
+*/
+uint32_t Systronix_PCA9548A::reset_bus_count_read (void)
+{
+	return _wire.resetBusCountRead();
+}
+
+
+//---------------------------< T A L L Y _ T R A N S A C T I O N >--------------------------------------------
+/**
+Here we tally errors.  This does not answer the what-to-do-in-the-event-of-these-errors question; it just
+counts them.
+
+TODO: we should decide if the correct thing to do when slave does not ack, or arbitration is lost, or
+timeout occurs, or auto reset fails (states 2, 5 and 4, 7 ??? state numbers may have changed since this
+comment originally added) is to declare these addresses as non-existent.
+
+We need to decide what to do when those conditions occur if we do not declare the device non-existent.
+When a device is declared non-existent, what do we do then? (this last is more a question for the
+application than this library).  The questions in this TODO apply equally to other i2c libraries that tally
+these errors.
+
+Don't set error.exists = false here! These errors are likely recoverable. bab & wsk 170612
+
+This is the only place we set error.error_val()
+
+TODO use i2c_t3 error or status enumeration here in the switch/case
+*/
+
+void Systronix_PCA9548A::tally_transaction (uint8_t value)
+	{
+	if (value && (error.total_error_count < UINT64_MAX))
+		error.total_error_count++; 			// every time here incr total error count
+
+	error.error_val = value;
+
+	switch (value)
+		{
+		case SUCCESS:
+			if (error.successful_count < UINT64_MAX)
+				error.successful_count++;
+			break;
+		case 1:								// i2c_t3 and Wire: data too long from endTransmission() (rx/tx buffers are 259 bytes - slave addr + 2 cmd bytes + 256 data)
+			error.data_len_error_count++;
+			break;
+#if defined I2C_T3_H
+		case I2C_TIMEOUT:
+			error.timeout_count++;			// 4 from i2c_t3; timeout from call to status() (read)
+#else
+		case 4:
+			error.other_error_count++;		// i2c_t3 and Wire: from endTransmission() "other error"
+#endif
+			break;
+		case 2:								// i2c_t3 and Wire: from endTransmission()
+		case I2C_ADDR_NAK:					// 5 from i2c_t3
+			error.rcv_addr_nack_count++;
+			break;
+		case 3:								// i2c_t3 and Wire: from endTransmission()
+		case I2C_DATA_NAK:					// 6 from i2c_t3
+			error.rcv_data_nack_count++;
+			break;
+		case I2C_ARB_LOST:					// 7 from i2c_t3; arbitration lost from call to status() (read)
+			error.arbitration_lost_count++;
+			break;
+		case I2C_BUF_OVF:
+			error.buffer_overflow_count++;
+			break;
+		case I2C_SLAVE_TX:
+		case I2C_SLAVE_RX:
+			error.other_error_count++;		// 9 & 10 from i2c_t3; these are not errors, I think
+			break;
+		case WR_INCOMPLETE:					// 11; Wire.write failed to write all of the data to tx_buffer
+			error.incomplete_write_count++;
+			break;
+		case SILLY_PROGRAMMER:				// 12
+			error.silly_programmer_error++;
+			break;
+		default:
+			error.unknown_error_count++;
+			break;
+		}
+	}
+
+
+//---------------------------< C O N T R O L _ W R I T E >----------------------------------------------------
+/**
 Write to the 8-bit control register
 returns 0 if no error, positive values for NAK errors
 
@@ -168,73 +289,59 @@ Prevent setting more than one bit?
 
 */
 
-uint8_t Systronix_PCA9548A::controlWrite (uint8_t control)
+uint8_t Systronix_PCA9548A::control_write (uint8_t control)
 	{
-	Wire.beginTransmission (_base);			// base address
-	error.ret_val = Wire.write (control);			// write control reg
-	_control_reg = control;					// shadow copy to remember this setting
+	uint8_t ret_val;
 
-	if (1 != error.ret_val)
+	if (!error.exists)							// exit immediately if device does not exist
+		return ABSENT;
+
+	_wire.beginTransmission (_base);			// base address
+
+	ret_val = _wire.write (control);			// returns # of bytes written to i2c_t3 buffer
+	if (1 != ret_val)
 		{
-		error.ret_val = 0;
-		tally_errors (error.ret_val);			// buffer write length error
+		tally_transaction (WR_INCOMPLETE);		// set the error value
+		return FAIL;
 		}
-	else
+
+	ret_val = _wire.endTransmission();			// returns 0 if successful
+	if (SUCCESS != ret_val)
 		{
-		// no error in buffer write
-	  	error.ret_val = Wire.endTransmission();	
-		// return: 0=success, 1=data too long, 2=recv addr NACK, 3=recv data NACK, 4=other error
-		if (error.ret_val)
-			{
-#if defined I2C_T3_H					
-			error.ret_val = Wire.status();			// to get error value
-#endif			
-			tally_errors (error.ret_val);				// increment the appropriate counter
-			}
-		else
-			{
-				// completely successful
-				if (error.successful_count < UINT64_MAX) error.successful_count++;
-				_control_reg = control;					// shadow copy to remember this setting
-				return SUCCESS;
-			}
+		tally_transaction (ret_val);			// increment the appropriate counter
+		return FAIL;							// calling function decides what to do with the error
 		}
-	return !SUCCESS;
-	// TODO this could return with error.ret_val set to zero
+
+	_control_reg = control;						// shadow copy to remember this setting
+	return SUCCESS;
 	}
 
-/**--------------------------< C O N T R O L R E A D >------------------------------------------------------
+
+//---------------------------< C O N T R O L _ R E A D >------------------------------------------------------
+/**
 
   Read the 8-bit control register 
 
-  return 0 if no error, FAIL if error (detail by using error.ret_val, ABSENT if slave had prior NAK
+  return 0 if no error, FAIL if error (detail by using error.error_val, ABSENT if slave had prior NAK
 */
 
-uint8_t Systronix_PCA9548A::controlRead (uint8_t *data)
+uint8_t Systronix_PCA9548A::control_read (uint8_t *data_ptr)
 	{
-	uint8_t read_count;
+	uint8_t ret_val;
 
-#if defined I2C_T3_H	
-	read_count = Wire.requestFrom(_base, 1, I2C_STOP);
-	if (read_count != 1)
-#else
-	read_count = Wire.requestFrom(_base, (uint8_t)1, (uint8_t)1); 		// Wire lib version
-	if (read_count != 1 )		
-#endif		
+	if (!error.exists)						// exit immediately if device does not exist
+		return ABSENT;
+
+	if (1 != _wire.requestFrom (_base, 1, I2C_STOP))
 		{
-		Serial.printf("control read got %u bytes, not 1\r\n", read_count);
-#if defined I2C_T3_H	
-		error.ret_val = Wire.status();				// to get error value
-#else
-		// PJRC Wire lib
-		error.ret_val = 1;		// force value for data length error
-#endif
-		tally_errors (error.ret_val);					// increment the appropriate counter
-		return !SUCCESS;
+		ret_val = _wire.status();						// to get error value
+		tally_transaction (ret_val);					// increment the appropriate counter
+		return FAIL;
 		}
 
-	if (error.successful_count < UINT64_MAX) error.successful_count++;
-	*data = (uint8_t)Wire.read();
+	*data_ptr = _wire.readByte();
+
+	tally_transaction (SUCCESS);
 	return SUCCESS;
 	}
 
@@ -265,14 +372,14 @@ uint8_t Systronix_PCA9548A::enableManyTest (void)
 
 	Serial.printf("enableManyTest is not complete! Use not recommended!\r\n");
 
-	stat = controlRead(&control_original);
+	stat = control_read(&control_original);
 	if (SUCCESS != stat) return !SUCCESS;		// if we can't even read why continue?
 
 	// write test pattern
-	stat = controlWrite(test_write);
+	stat = control_write(test_write);
 	if (SUCCESS != stat) bad_count++;
 
-	stat = controlRead(&control_read_val);
+	stat = control_read(&control_read_val);
 	if (SUCCESS != stat) bad_count++;
 	if (test_write != control_read_val) 
 	{
@@ -283,100 +390,15 @@ uint8_t Systronix_PCA9548A::enableManyTest (void)
 	// dangerous, ~0x01 is 0xFE which enables 7 channels and any pullups on them,
 	// could prevent slave driving low against such a large pullup load
 	test_write = ~test_write;
-	stat = controlWrite(test_write);
+	stat = control_write(test_write);
 	if (SUCCESS != stat) return !SUCCESS;
 
-	stat = controlRead(&control_read_val);
+	stat = control_read(&control_read_val);
 	if (SUCCESS != stat) return !SUCCESS;
 	if (test_write != control_read_val) return !SUCCESS;
 
-	stat = controlWrite(control_original);
+	stat = control_write(control_original);
 	if (SUCCESS != stat) return !SUCCESS;
 
 	return SUCCESS;
 }
-
-
-
-//---------------------------< T A L L Y _ E R R O R S >------------------------------------------------------
-//
-// Here we tally errors.  This does not answer the 'what to do in the event of these errors' question; it just
-// counts them.  If the device does not ack the address portion of a transaction or if we get a timeout error,
-// exists is set to false.  We assume here that the timeout error is really an indication that the automatic
-// reset feature of the i2c_t3 library failed to reset the device in which case, the device no longer 'exists'
-// for whatever reason.
-//
-// Only call this method if there is some kind of error, since total is incremented on every entry here.
-/** 
-	TODO these error values don't match all the 0..9 status() enum e.g. timeout has a value of 3
-	part of the problem is mashing up status() codes and Wire.write() 
-
-	From 29Dec16 release i2c_t3.h, now 11 status codes
-	enum i2c_status   {I2C_WAITING,
-	                   I2C_SENDING,
-	                   I2C_SEND_ADDR,
-	                   I2C_RECEIVING,
-	                   I2C_TIMEOUT,
-	                   I2C_ADDR_NAK,
-	                   I2C_DATA_NAK,
-	                   I2C_ARB_LOST,
-	                   I2C_BUF_OVF,
-	                   I2C_SLAVE_TX,
-	                   I2C_SLAVE_RX};	
-
-	Detailed status (not all are errors!) 11 values enum 0..10 returned from status()
-		0..3 	I2C_WAITING, I2C_SENDING, I2C_RECEIVING, I2C_SEND_ADDR	- not errors, but cycle in progress when Teensy is master
-		4 		I2C_TIMEOUT, 								- error
-		5-6		I2C_ADDR_NAK, I2C_DATA_NAK, 				- errors
-		7		I2C_ARB_LOST, 								- error, sort of (in multi-master mode)
-		8 		I2C_BUF_OVF, 								- error
-		9-10	I2C_SLAVE_TX, I2C_SLAVE_RX;					- not errors? Only apply to Teensy slave mode?
-	0..2 can't happen if status() called after endTransmission since it blocks until cycle is complete
-
-	If we are not using i2c_t3, then Wire errors are not so well-defined:
-	0=success (we change this to mean incomplete write, 1=data too long, 2=recv addr NACK, 3=recv data NACK, 4=other error
-*/
-
-void Systronix_PCA9548A::tally_errors (uint8_t err)
-	{
-	if (error.total_error_count < UINT64_MAX) error.total_error_count++; 	// every time here incr total error count
-	switch (err)
-		{
-		case 0:					// Wire.write failed to write all of the data to tx_buffer
-			error.incomplete_write_count++;
-			break;
-		case 1:					// data too long from endTransmission() (rx/tx buffers are 259 bytes - slave addr + 2 cmd bytes + 256 data)
-			error.data_len_error_count++;
-			break;
-		case 4:					 
-#if defined I2C_T3_H		
-			error.timeout_count++;			// error 4 = i2c_t3 timeout
-#else
-			error.other_error_count++;		// error 4 = Wire "other error"
-#endif
-			break;
-		case 2:
-		case 5:
-			error.rcv_addr_nack_count++;
-			break;
-		case 3:
-		case 6:
-			error.rcv_data_nack_count++;
-			break;
-		case 7:					// arbitration lost from call to status() (read)
-			error.arbitration_lost_count++;
-			break;
-		case 8:
-			error.buffer_overflow_count++;
-			break;
-		case 9:
-		case 10:
-			error.other_error_count++;		// i2c_t3 these are not errors, I think
-			break;
-		case 11:
-		default:
-			error.unknown_error_count++;
-			break;
-		}
-
-	}
